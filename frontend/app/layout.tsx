c5ae9c6d5568eb95f5029c17ae8064747e086c38
Tony Lee <tonyklee@umich.edu>
2026-06-09 22:41:28 -0400

    adding favicon
    Signed-off-by: Tony Lee <tonyklee@umich.edu>

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,24 +1,25 @@
-import type React from "react"
-import type { Metadata } from "next"
-import "./globals.css"
-
+import type React from "react";
+import type { Metadata } from "next";
+import "./globals.css";
 export const metadata: Metadata = {
-  title: "gittuf Metadata Visualizer",
-  description: "Visualize and analyze gittuf's security metadata structure across repository commits",
+  title: "Gittuf Visualizer",
+  description:
+    "Visualize and analyze gittuf's security metadata structure across repository commits",
   generator: "v0.dev",
+  applicationName: "Gittuf Visualizer",
   icons: {
-    icon: "/../assets/Logo.png",
+    icon: "/favicon.png",
   },
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
